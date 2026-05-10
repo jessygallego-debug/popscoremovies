c2eafa696cbd3acde63cba34f80043fe51b130e2
Jessy Gallego <jessygallego@Jessys-MacBook-Pro.local>
2026-05-10 14:31:28 -0400

    Restore PopScore icon background

--- app/components/popscore-display.tsx
+++ app/components/popscore-display.tsx
@@ -76,7 +76,7 @@ export default function PopScoreDisplay({
   return (
     <div className={className}>
       <div className="flex items-center gap-2">
-        <span className="relative block h-8 w-8 overflow-hidden rounded-lg">
+        <span className="relative block h-8 w-8 overflow-hidden rounded-lg bg-black">
           <Image
             src={popScoreTitle.iconSrc}
             alt={`${popScoreTitle.label} PopScore icon`}
